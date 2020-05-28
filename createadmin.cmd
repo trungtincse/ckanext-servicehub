8e18219e5f06a9ec670fd537b2be95ed69ff8dce
trungtincse <trungtincse@gmail.com>
2020-05-28 13:48:10 +0700

    create user seanh

--- createadmin.cmd
+++ createadmin.cmd
@@ -1,4 +1,4 @@
-#! bin/bash
+#! /bin/bash
 . /usr/lib/ckan/default/bin/activate &&\
 cd /usr/lib/ckan/default/src/ckan &&\
 paster sysadmin add seanh password=12345678 email=tin@g.g apikey=aee93875-75c7-481a-aa11-27c9bb72a2bf -c /etc/ckan/default/development.ini

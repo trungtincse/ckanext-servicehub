d2d32f7b914bf7b25ff5072a725131c295be1d0e
trungtincse <trungtincse@gmail.com>
2020-06-24 21:32:00 +0700

    fix code

--- ckanext/servicehub/action/delete.py
+++ ckanext/servicehub/action/delete.py
@@ -27,22 +27,26 @@ fileserver_host = config.get('ckan.servicehub.fileserver_host')
 central_logger = logging.getLogger('logserver')
 local_logger = logging.getLogger('local')
 
+
 def service_delete(context, data_dict):
     app_id = data_dict['id']
-    _check_access(u'delete_service',context,dict(app_id=app_id))
+    _check_access(u'delete_service', context, dict(app_id=app_id))
     session = context['session']
     try:
         session.query(App).filter(App.app_id == app_id).delete()
         app_solr.delete_app(app_id)
         session.commit()
-    except:
+    except Exception as ex:
+        print ex.message
         central_logger.info("user=%s&action=service_delete&error_code=1" % context['user'])
-        local_logger.info("%s %s %s" % (context['user'], "service_delete", "Can not delete application %s"%app_id))
-        return dict(success=False,error="Can not delete application %s"%app_id)
+        local_logger.info("%s %s %s" % (context['user'], "service_delete", "Can not delete application %s" % app_id))
+        session.rollback()
+        return dict(success=False, error="Can not delete application %s" % app_id)
     logic.get_action('app_index_delete')(context, {'app_id': app_id})
     central_logger.info("user=%s&action=service_delete&error_code=0" % context['user'])
-    local_logger.info("%s %s %s"%(context['user'],"service_delete","Delete app %s success"%app_id))
+    local_logger.info("%s %s %s" % (context['user'], "service_delete", "Delete app %s success" % app_id))
     return dict(success=True)
+
 
 # def call_delete(context, data_dict):
 #     call_id = data_dict['call_id']
@@ -52,24 +56,7 @@ def service_delete(context, data_dict):
 #     output_delete(context,data_dict)
 #     session.commit()
 #     return dict(success=True)
-#
-#
-# def reqform_delete(context, data_dict):
-#     path = os.path.join(fileserver_host, 'requestform', data_dict['app_id'])
-#     return http_session.delete(path).json()
-#
-#
-# def input_delete(context, data_dict):
-#     path = os.path.join(fileserver_host, 'input', data_dict['call_id'])
-#     return http_session.delete(path).json()
-#
-#
-# def output_delete(context, data_dict):
-#     path = os.path.join(fileserver_host, 'output', data_dict['call_id'])
-#     return http_session.delete(path).json()
 
-def file_delete(context, data_dict):
-    pass
 public_functions = dict(service_delete=service_delete,
                         # call_delete=call_delete,
                         # reqform_delete=reqform_delete,
